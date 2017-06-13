e6586dd521af8fd0d74d8c9fee5b81e23d1a648e
alilaa <anton@alila.se>
2017-06-13 18:09:38 +0200

    Updated version information.

--- DotaDota/Properties/AssemblyInfo.cs
+++ DotaDota/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alila Incorporated")]
 [assembly: AssemblyProduct("DotaDota")]
-[assembly: AssemblyCopyright("Copyright ©  20178")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
